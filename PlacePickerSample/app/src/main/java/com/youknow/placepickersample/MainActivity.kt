c8248d28d5f393cf58e07394c727779e7dcadf42
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2019-06-03 14:25:18 +0900

    Map view 기본 구현 및 API Key 추가

--- PlacePickerSample/app/src/main/java/com/youknow/placepickersample/MainActivity.kt
+++ PlacePickerSample/app/src/main/java/com/youknow/placepickersample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.youknow.placepickersample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlacePicker.setOnClickListener { showPlacePicker() }
+    }
+
+    private fun showPlacePicker() {
+        startActivity(Intent(this, PlacePickerActivity::class.java))
     }
 }
---
+++ PlacePickerSample/app/src/main/java/com/youknow/placepickersample/PlacePickerActivity.kt
@@ -0,0 +1,42 @@
+package com.youknow.placepickersample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_place_picker.*
+
+class PlacePickerActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_picker)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        btnSelectLocation.setOnClickListener { selectLocation() }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun selectLocation() {
+        Toast.makeText(this, "Select!", Toast.LENGTH_SHORT).show()
+    }
+}
