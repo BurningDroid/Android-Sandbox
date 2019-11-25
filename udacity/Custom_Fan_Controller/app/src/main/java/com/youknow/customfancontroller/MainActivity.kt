5c575c69fe96bb091d07c3644aa647f964cc76ea
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-11-25 15:20:22 +0900

    Udacity - Custom View

--- udacity/Custom_Fan_Controller/app/src/main/java/com/youknow/customfancontroller/MainActivity.kt
+++ udacity/Custom_Fan_Controller/app/src/main/java/com/youknow/customfancontroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.youknow.customfancontroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
