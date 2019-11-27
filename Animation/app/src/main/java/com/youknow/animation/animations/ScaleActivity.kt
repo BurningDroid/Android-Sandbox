67f1e0e558a57b1a652889dedb54dbe1de09f9cf
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-11-27 16:49:40 +0900

    Animation - scale

--- Animation/app/src/main/java/com/youknow/animation/animations/ScaleActivity.kt
+++ Animation/app/src/main/java/com/youknow/animation/animations/ScaleActivity.kt
@@ -1,11 +1,35 @@
 package com.youknow.animation.animations
 
-import com.youknow.animation.base.BaseAnimationActivity
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.youknow.animation.R
+import kotlinx.android.synthetic.main.activity_scale.*
 
-class ScaleActivity : BaseAnimationActivity() {
+class ScaleActivity : AppCompatActivity() {
 
-    override fun animate() {
+    private var scaleLevel = 2F
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scale)
+
+        btnScale.setOnClickListener {
+            animate(scaleLevel)
+            if (scaleLevel == 2F) {
+                scaleLevel = 1F
+                btnScale.setImageResource(R.drawable.ic_zoom_out)
+            } else {
+                scaleLevel = 2F
+                btnScale.setImageResource(R.drawable.ic_zoom_in)
+            }
+        }
+    }
+
+    private fun animate(scaleLevel: Float) {
+        ObjectAnimator.ofFloat(ivSnow, View.SCALE_X, scaleLevel).start()
+        ObjectAnimator.ofFloat(ivSnow, View.SCALE_Y, scaleLevel).start()
     }
 
 }
