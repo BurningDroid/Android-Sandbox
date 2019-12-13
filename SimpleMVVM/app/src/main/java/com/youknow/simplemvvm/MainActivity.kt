432078653fd0f02174c64e5d0b64cb2e6963bce8
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-12-13 18:51:26 +0900

    mvvm 작업 중

--- SimpleMVVM/app/src/main/java/com/youknow/simplemvvm/MainActivity.kt
+++ SimpleMVVM/app/src/main/java/com/youknow/simplemvvm/MainActivity.kt
@@ -2,11 +2,15 @@ package com.youknow.simplemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.youknow.simplemvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = MainViewModel()
     }
 }
---
+++ SimpleMVVM/app/src/main/java/com/youknow/simplemvvm/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.youknow.simplemvvm
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel {
+    private val _name = MutableLiveData("Ada")
+    private val _lastName = MutableLiveData("Lovelace")
+    private val _count =  MutableLiveData(0)
+
+    val name: LiveData<String> = _name
+    val lastName: LiveData<String> = _lastName
+    val count: LiveData<Int> = _count
+
+    fun increaseCount() {
+        _count.value = (_count.value ?: 0) + 1
+        Log.d("TEST", "increaseCount: $count")
+    }
+
+}
