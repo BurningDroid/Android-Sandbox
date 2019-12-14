0400ee96fd7ff497089bfbd5df00554576b02de9
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2019-12-14 23:39:43 +0900

    codelab done (#9)

---
+++ codelab/Android Room with a View/app/src/main/java/com/youknow/codelabroom/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.youknow.codelabroom
+
+import android.app.Application
+import android.content.Context
+
+class MainApplication : Application() {
+
+    companion object {
+        lateinit var appContext: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+}
