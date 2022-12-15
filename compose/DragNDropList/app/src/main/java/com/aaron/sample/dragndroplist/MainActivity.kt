7a8b65f209a1cf6f46f2f5ecc537e5c2808dd0fa
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-12-15 18:03:47 +0900

    base ui

--- compose/DragNDropList/app/src/main/java/com/aaron/sample/dragndroplist/MainActivity.kt
+++ compose/DragNDropList/app/src/main/java/com/aaron/sample/dragndroplist/MainActivity.kt
@@ -3,13 +3,27 @@ package com.aaron.sample.dragndroplist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aaron.sample.dragndroplist.ui.theme.DragNDropListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyScreen()
                 }
             }
         }
@@ -30,14 +44,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MyScreen() {
+    val users = (1..100).map { User(it, "Name: $it") }
+    LazyColumn(
+    ) {
+        itemsIndexed(users) { _, user ->
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row() {
+                        Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_person), contentDescription = "")
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text("${user.id}")
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(user.name)
+                    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DragNDropListTheme {
-        Greeting("Android")
+                    Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_menu), contentDescription = "")
+                }
+
+                Divider()
+            }
+        }
     }
 }
---
+++ compose/DragNDropList/app/src/main/java/com/aaron/sample/dragndroplist/User.kt
@@ -0,0 +1,6 @@
+package com.aaron.sample.dragndroplist
+
+data class User(
+    val id: Int,
+    val name: String,
+)
