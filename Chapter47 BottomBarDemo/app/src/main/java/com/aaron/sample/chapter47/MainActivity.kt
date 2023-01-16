87d4af0684fd41c65b3903359ddf249cf1b961e3
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2023-01-16 23:14:33 +0900

    project init

---
+++ Chapter47 BottomBarDemo/app/src/main/java/com/aaron/sample/chapter47/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.aaron.sample.chapter47
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aaron.sample.chapter47.ui.theme.Chapter47BottomBarDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Chapter47BottomBarDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+
+}
