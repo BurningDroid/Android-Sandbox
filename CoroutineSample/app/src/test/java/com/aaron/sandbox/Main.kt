eb75a9c0b0b68086379576bbbc894a256d0128c5
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2024-04-08 22:34:59 +0900

    circle animation

--- CoroutineSample/app/src/test/java/com/aaron/sandbox/Main.kt
+++ CoroutineSample/app/src/test/java/com/aaron/sandbox/Main.kt
@@ -1,5 +1,57 @@
 package com.aaron.sandbox
 
-fun main() {
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.runBlocking
 
+suspend fun main(): Unit = runBlocking {
+    val api = FakeUserApi()
+    val users = allUsersFlow(api)
+    users
+        .onStart {
+            println("show progress")
+        }
+        .onCompletion {
+            println("hide progress")
+        }
+        .onEach {
+        println("onEach: $it")
+    }.collect {
+        println("collect")
+    }
+}
+
+fun allUsersFlow(api: UserApi): Flow<User> = channelFlow {
+    var page = 0
+    do {
+        val users = api.takePage(page++)
+//        emitAll(users.asFlow())
+        users.forEach { send(it) }
+    } while (!users.isNullOrEmpty())
+}
+
+
+data class User(val name: String)
+
+interface UserApi {
+    suspend fun takePage(pageNumber: Int): List<User>
+}
+
+class FakeUserApi: UserApi {
+    private val users = (0..20).map { User("User_$it") }
+    private val pageSize = 1
+
+    override suspend fun takePage(pageNumber: Int): List<User> {
+        delay(100L)
+        return users.drop(pageSize * pageNumber)
+            .take(pageSize)
+    }
 }
---
+++ compose/Inflearn Animation/app/src/main/java/com/aaron/inflearnanimation/Ex1_12.kt
@@ -0,0 +1,90 @@
+package com.aaron.inflearnanimation
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Ex1_12() {
+    Column(
+        modifier = Modifier.padding(20.dp)
+    ) {
+        var sliderProgress by remember { mutableStateOf(0.5F) }
+        Slider(
+            value = sliderProgress,
+            onValueChange = {
+                sliderProgress = it
+            },
+            colors = SliderDefaults.colors(
+                thumbColor = Color.Red,
+                activeTrackColor = Color.Black,
+            ),
+        )
+
+        SliderCircle(
+            sliderProgress = sliderProgress
+        )
+    }
+}
+
+@Composable
+fun SliderCircle(
+    sliderProgress: Float
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier.size(250.dp)
+        ) {
+            drawArc(
+                brush = SolidColor(Color.Gray),
+                startAngle = 0F,
+                sweepAngle = 360F,
+                useCenter = false,
+                style = Stroke(35F)
+            )
+
+            val slideChangedProgress = sliderProgress * 360
+            drawArc(
+                brush = SolidColor(Color.Blue),
+                startAngle = 0F,
+                sweepAngle = slideChangedProgress,
+                useCenter = false,
+                style = Stroke(35F, cap = StrokeCap.Round)
+            )
+
+        }
+
+        Text(
+            text = "${(sliderProgress * 100).toInt()}%",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+    }
+}
