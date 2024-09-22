1d1d63a9515f4e29524f81d1e1916f3a62c93bac
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2024-09-22 20:15:45 +0900

    Update HomePane.kt

--- koin_sample2/app/src/main/java/com/aaron/kointest/ui/home/HomePane.kt
+++ koin_sample2/app/src/main/java/com/aaron/kointest/ui/home/HomePane.kt
@@ -1,10 +1,14 @@
 package com.aaron.kointest.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aaron.kointest.ui.theme.KoinTestTheme
@@ -14,10 +18,20 @@ fun HomePane(
     vm: HomeViewModel = HomeViewModel()
 ) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = vm.greeting,
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Greeting(
+                name = vm.greeting,
+                modifier = Modifier.padding(innerPadding)
+            )
+
+            Button(onClick = {}) {
+                Text("Button")
+            }
+        }
     }
 }
 
